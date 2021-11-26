ba2028330ca6e30bdcaa20b8181cbed6e9eafee6
Guan Xi Chen <michael.chen25@GCCYJTPBJ1WK.local>
2021-11-26 09:50:20 -0500

    finished first half

--- myGame.py
+++ myGame.py
@@ -6,10 +6,17 @@ lib.clearConsole()
 #Challenge 1 ----------------------------
 
 lib.printDivider("-",10)
-print("Jabari stands on the slippery deck of the pool.\nIn front of him stands a tall diving board.\nTo his left, three children his age are playing together in the water.\nHis dad stands behind him, smiling encouragingly.\nWhat will he do?")
+print("Jabari stands on the slippery deck of the pool with his father behind him, eager to play.")
+time.sleep(1)
+print("Some other children play in the kiddie pool to his left.")
+time.sleep(1)
+print("The main pool stands in front of him.")
+time.sleep(1)
 lib.printDivider("-",10)
-print("Options: Go left, Go forward, Go backwards")
+print("Options: left, forward")
 lib.printDivider("-",10)
+allowDadCounter = 0
+repeatCounter = 0
 chooseAgain = 'True'
 while chooseAgain == "True":
     actionCounter = 0
@@ -29,20 +36,54 @@ while chooseAgain == "True":
         actionCounter == 0
     elif "left" in jabariAction:
         lib.printDivider("-",10)
-        print("Jabari goes towards the children, but they all glare at him coldly.\nChoose another option: (Forward, Backwards)")
+        if repeatCounter == 0:
+            print("Jabari goes and plays with the other children in the kiddie pool for a while.")
+        else:
+            print("Jabari goes and plays with the children in the kiddie pool some more.")
+        time.sleep(1)
+        allowDadCounter += 1
+        if allowDadCounter == 1:
+            print("Jabari is slightly tired, but wishes to play some more.")
+            time.sleep(1)
+            lib.printDivider("-",10)
+            print("Choose another option: left, forward")
+        else:
+            print("Jabari feels slightly bored. Maybe he should ask his father about what to do next.")
+            time.sleep(1)
+            lib.printDivider("-",10)
+            print("Choose another option: forward, backwards")
         lib.printDivider("-",10)
         actionCounter == 0
+        repeatCounter += 1
+        chooseAgain = "True"
     elif "forward" in jabariAction:
         lib.printDivider("-",10)
-        print("Jabari awkwardly climbs on top of the diving board, and decides to take a leap of faith.")
+        if repeatCounter == 0:
+            print("Jabari goes and plays around in the main pool, swimming around the place.")
+        else:
+            print("Jabari goes and swims in the main pool some more.")
+        time.sleep(1)
+        allowDadCounter += 1
+        if allowDadCounter == 1:
+            print("Jabari is slightly tired, but wishes to play some more.")
+            time.sleep(1)
+            lib.printDivider("-",10)
+            print("Choose another option: left, forward")
+        else:
+            print("Jabari feels slightly bored. Maybe he should ask his father about what to do next.")
+            time.sleep(1)
+            lib.printDivider("-",10)
+            print("Choose another option: left, backwards")
+            lib.printDivider("-",10)
+        actionCounter == 0
+        repeatCounter += 1
+        chooseAgain = "True"
+    elif allowDadCounter == 2 and "backwards" in jabariAction:
+        lib.printDivider("-",10)
+        print("Jabari runs back towards his father, and he insists that Jabari should try something new.")
         lib.printDivider("-",10)
         actionCounter == 0
         chooseAgain = "False"
-    elif "backwards" in jabariAction:
-        lib.printDivider("-",10)
-        print("Jabari runs back towards his father, but he insists that he should try something new.\nChoose another option: (Left, Forward)")
-        lib.printDivider("-",10)
-        actionCounter == 0
     else:
         lib.printDivider("-",10)
         print("I don't recognize those words,\nTry again.")
